d80fb633d03cb293bf47fb2952ee5ffe1ae9c129
NoobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-03-09 18:54:49 +0330

    remove setup because for problem first config
    Signed-off-by: NoobforAl <58856931+NoobforAl@users.noreply.github.com>

--- comandor/log.py
+++ comandor/log.py
@@ -2,6 +2,3 @@ import logging as log
 
 FORMAT: str = '%(asctime)s : %(levelname)s : %(name)s : %(message)s'
 DATEFMT: str = '%d-%b-%y %H:%M:%S'
-
-# Setup basics setting logger
-log.basicConfig(level=log.INFO, format=FORMAT, datefmt=DATEFMT)
