35a37246eafe192d30d4b1bf068131fe4d23c84b
noobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-07-29 19:45:54 +0330

    add app componet and fix main file
    Signed-off-by: noobforAl <58856931+NoobforAl@users.noreply.github.com>

--- comandor/main.py
+++ comandor/main.py
@@ -1,16 +1,17 @@
 from tqdm.contrib.logging import logging_redirect_tqdm
+
 from comandor.settings import newConfig, Setting
 from comandor.args import read_args
 from comandor.log import log
-
 from comandor.app import App
 
 
 if __name__ == "__main__":
     args = read_args()
     setting: Setting = newConfig(*args)
+    app = App(setting)
 
     log.info(f"start commander -> {setting.name}")
 
     with logging_redirect_tqdm():
-        app = App(setting)
+        app.Run()
