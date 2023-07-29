40a8277196565a034567716678320d71ec676b14
noobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-07-29 20:44:37 +0330

    fix test file
    Signed-off-by: noobforAl <58856931+NoobforAl@users.noreply.github.com>

--- test/test_comandor.py
+++ test/test_comandor.py
@@ -1,45 +1,27 @@
-from comandor import settings
-from comandor import models
-from comandor import main
-
+from comandor.settings import newConfig
+from comandor.app import App
 
 import unittest
+import os
 
 
 class TestCore(unittest.TestCase):
 
-    def test_loadSetting(self):
-        with self.assertRaises(settings.ValidationError):
-            settings.loadSetting("./test/comandor_break.json")
+    def test_ymlAction(self):
+        App(newConfig(
+            "./log.log",
+            "test_comandor.yml",
+            True,
+            "",
+        )).Run()
 
-    def test_modelAction(self):
-        # raise error for commands type
-        with self.assertRaises(settings.ValidationError):
-            models.Action(
-                action_name="test ",
-                path=".",
-                commands=""
-            )
-
-    def test_modelSetting(self):
-        # raises error for action type
-        with self.assertRaises(settings.ValidationError):
-            models.Setting(
-                name="test ",
-                actions=[""]
-            )
-
-    def test_main(self):
-        setting = main.newConfig(
-            logfile="",
-            config="./test/test_comandor.json",
-            debug=True
-        )
-        # if action return 1 status code assert!
-        try:
-            main.runActions(setting.actions)
-        except Exception as e:
-            self.assert_(e)
+    def test_Skip(self):
+        App(newConfig(
+            "./log.log",
+            "test_comandor.json",
+            True,
+            "test",
+        )).Run()
 
 
 if __name__ == '__main__':
