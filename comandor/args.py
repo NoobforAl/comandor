8eb8e97866223d3d1fa1f08d9374defe6b7f6022
noobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-07-29 19:33:47 +0330

    fix args: arg skip name
    Signed-off-by: noobforAl <58856931+NoobforAl@users.noreply.github.com>

--- comandor/args.py
+++ comandor/args.py
@@ -1,9 +1,9 @@
-from typing import Tuple, List
+from typing import Tuple
 
 import argparse
 
 
-def read_args() -> Tuple[str, str, bool, List[str, str]]:
+def read_args() -> Tuple[str, str, bool, str]:
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-l', "--logfile", type=str,
@@ -12,14 +12,14 @@ def read_args() -> Tuple[str, str, bool, List[str, str]]:
                         help='where you have config file')
     parser.add_argument('-d', "--debug", action='store_true',
                         required=False, help='run debug mod')
-    parser.add_argument('-sk', "--skip", default="0-0",
+    parser.add_argument('-sk', "--skip", default="",
                         required=False, help='run debug mod')
 
     args = parser.parse_args()
     logfile: str = args.logfile
     config: str = args.config
     debug: bool = args.debug
-    skip: List[str, str] = args.skip.split("-")
+    skip: str = args.skip
     return (logfile, config, debug, skip)
 
 
