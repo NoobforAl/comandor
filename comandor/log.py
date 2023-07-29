c4b57cbd67354787d912adb79882707adfe80145
noobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-07-29 19:14:58 +0330

    add __all__ log for export values
    Signed-off-by: noobforAl <58856931+NoobforAl@users.noreply.github.com>

--- comandor/log.py
+++ comandor/log.py
@@ -2,3 +2,9 @@ import logging as log
 
 FORMAT: str = '%(asctime)s : %(levelname)s : %(name)s : %(message)s'
 DATEFMT: str = '%d-%b-%y %H:%M:%S'
+
+__all__ = [
+    "log",
+    "FORMAT",
+    "DATEFMT",
+]
