bb9e15ed5e2200edfc6e165f9535108f908580fc
noobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-07-29 19:39:48 +0330

    fix setting componet and add yaml suport
    Signed-off-by: noobforAl <58856931+NoobforAl@users.noreply.github.com>

--- comandor/settings.py
+++ comandor/settings.py
@@ -1,30 +1,83 @@
-from pydantic import ValidationError
-
+from comandor.log import log, FORMAT, DATEFMT
 from comandor.models import Setting
-from comandor.log import log
 
+from pydantic import ValidationError
+from typing import List
+
+import yaml
 import json
+import sys
 import os
 
 
-# load setting form file and return setting
-# default use ./.comandor
 def loadSetting(file: str = ".comandor") -> Setting:
     if not os.path.exists(file):
         raise Exception("Config file not found!")
 
     setting: Setting
+    op: dict[str, str]
+
     with open(file, "r") as f:
         try:
             op = json.load(f)
-            setting = Setting(**op)
-
-        except ValidationError as e:
-            log.error(e)
-            raise e
 
         except json.JSONDecodeError as e:
-            log.error(e)
-            raise e
+
+            try:
+                op = yaml.safe_load(f)
+            except yaml.error.YAMLError as err:
+                log.error("pars conf error", e, err)
+                raise
+
+    try:
+        setting = Setting(**op)
+    except ValidationError as e:
+        log.error(e)
+        raise e
 
     return setting
+
+
+def newConfig(
+        logfile: str, config: str,
+        debug: bool, skip: str) -> Setting:
+
+    setting: Setting = loadSetting(config)
+    level: int = log.INFO
+    handlers: List = []
+
+    if debug or setting.debug:
+        level = log.DEBUG
+
+    if logfile or setting.logfile:
+        filename = logfile or str(setting.logfile)
+        filemode = "a"
+        handlers = [
+            log.FileHandler(filename, filemode),
+            log.StreamHandler(sys.stdout)
+        ]
+
+    log.basicConfig(
+        level=level,
+        format=FORMAT,
+        datefmt=DATEFMT,
+        handlers=handlers)
+
+    if debug or setting.debug:
+        log.debug("run debug mode!")
+
+    if skip != "":
+        log.warn(f"remove action with this match: {skip}")
+        for ac in setting.actions:
+            if ac.action_name.find(skip) != -1:
+                setting.actions.remove(ac)
+
+    log.debug("logger configure!")
+    log.debug("loaded Setting!")
+    return setting
+
+
+__all__ = [
+    "newConfig",
+    "Setting",
+]
